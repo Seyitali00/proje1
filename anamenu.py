7a978f0d3b29a5c101b38099cb5425c71fae7515
Seyitali00 <seyitalikayar14@gmail.com>
2025-05-04 17:15:06 +0300

    proje güncel

--- anamenu.py
+++ anamenu.py
@@ -1,15 +1,22 @@
-print("\33[1;31;40m")
-print("╔═════════════════════╗")
-print("║                     ║")   
-print("║  1-Hesap Makinesi   ║")
-print("║  2-Oyunlar          ║")
-print("║  3-Çizim            ║")
-print("║  4-Çikis yap        ║")
-print("║                     ║")
-print("║    Seçim yapin      ║") 
-print("╚═════════════════════╝")
-secim=input("Seçiminizi yapiniz : ")  
-if secim == "1" : print("Hesap Makinesi seçildi")
-import aramenu
-if secim == "2" : print("Oyunlar Secildi")
 import aramenu2
+import aramenu
+def anamenu():
+    print("\33[1;31;40m")
+    print("╔═════════════════════╗")
+    print("║                     ║")   
+    print("║  1-Hesap Makinesi   ║")
+    print("║  2-Oyunlar          ║")
+    print("║  3-Çizim            ║")
+    print("║  4-Çikis yap        ║")
+    print("║                     ║")
+    print("║    Seçim yapin      ║") 
+    print("╚═════════════════════╝")
+    secim=input("Seçiminizi yapiniz : ")  
+    if secim == "1" :
+        print("Hesap Makinesi seçildi")
+        aramenu.aramenu()
+        anamenu()
+    if secim == "2" : 
+        print("Oyunlar Secildi")
+        aramenu2.menu2()
+        anamenu()
