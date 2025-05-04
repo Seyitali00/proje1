e3c18d53f4ebf053af8962414c540f3fd8b45e29
Seyitali00 <seyitalikayar14@gmail.com>
2025-05-04 14:45:50 +0300

    proje güncellemesi

---
+++ anamenu.py
@@ -0,0 +1,13 @@
+print("\33[1;31;40m")
+print("╔═════════════════════╗")
+print("║                     ║")   
+print("║  1-Hesap Makinesi   ║")
+print("║  2-Oyunlar          ║")
+print("║  3-Çizim            ║")
+print("║  4-Çikis yap        ║")
+print("║                     ║")
+print("║    Seçim yapin      ║") 
+print("╚═════════════════════╝")
+secim=input("Seçiminizi yapiniz : ")  
+if secim == "1" : print("Hesap Makinesi seçildi")
+import aramenu
